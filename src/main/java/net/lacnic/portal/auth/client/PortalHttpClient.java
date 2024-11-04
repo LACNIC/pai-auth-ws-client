63214eddc91cb47476573bd777dcd1b11435b8ad
Gerardo Rada <gerardorada84@gmail.com>
2024-11-03 23:09:54 -0300

    no message

--- src/main/java/net/lacnic/portal/auth/client/PortalHttpClient.java
+++ src/main/java/net/lacnic/portal/auth/client/PortalHttpClient.java
@@ -3,14 +3,15 @@ package net.lacnic.portal.auth.client;
 import java.security.KeyStore;
 
 import org.apache.http.HttpVersion;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.params.ClientPNames;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.CoreConnectionPNames;
@@ -21,7 +22,38 @@ import org.apache.http.protocol.HTTP;
 @SuppressWarnings("deprecation")
 public class PortalHttpClient {
 
-	public static HttpClient getNewHttpClient() {
+//	public static HttpClient getNewHttpClient2() {
+//		try {
+//			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+//			trustStore.load(null, null);
+//
+//			MySSLSocketFactory sf = new MySSLSocketFactory(trustStore);
+//			sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+//
+//			HttpParams params = new BasicHttpParams();
+//			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+//			HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+//
+//			SchemeRegistry registry = new SchemeRegistry();
+//			registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+//			registry.register(new Scheme("https", sf, 443));
+//
+//			ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
+//
+//			DefaultHttpClient httpClient = new DefaultHttpClient(ccm, params);
+//
+//			int timeout = 40; // seconds
+//			HttpParams httpParams = httpClient.getParams();
+//			httpParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, timeout * 1000);
+//			httpParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, timeout * 1000);
+//			httpParams.setParameter(ClientPNames.CONN_MANAGER_TIMEOUT, new Long(timeout * 1000));
+//			return httpClient;
+//		} catch (Exception e) {
+//			return new DefaultHttpClient();
+//		}
+//	}
+
+	public static CloseableHttpClient getNewHttpClient() {
 		try {
 			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
 			trustStore.load(null, null);
@@ -39,16 +71,17 @@ public class PortalHttpClient {
 
 			ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
 
-			DefaultHttpClient httpClient = new DefaultHttpClient(ccm, params);
+			CloseableHttpClient httpClient = new DefaultHttpClient(ccm, params);
 
 			int timeout = 40; // seconds
 			HttpParams httpParams = httpClient.getParams();
 			httpParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, timeout * 1000);
 			httpParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, timeout * 1000);
-			httpParams.setParameter(ClientPNames.CONN_MANAGER_TIMEOUT, new Long(timeout * 1000));
+			httpParams.setParameter(ClientPNames.CONN_MANAGER_TIMEOUT, (long) timeout * 1000);
+
 			return httpClient;
 		} catch (Exception e) {
-			return new DefaultHttpClient();
+			return HttpClients.createDefault(); // Returns a default CloseableHttpClient instance
 		}
 	}
 }
