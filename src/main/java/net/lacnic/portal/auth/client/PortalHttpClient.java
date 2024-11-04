9808df4cc4330c9fd4fdaf259f360c52430ac5e6
Gerardo Rada <gerardorada84@gmail.com>
2024-11-04 00:25:32 -0300

    no message

--- src/main/java/net/lacnic/portal/auth/client/PortalHttpClient.java
+++ src/main/java/net/lacnic/portal/auth/client/PortalHttpClient.java
@@ -10,9 +10,7 @@ import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.CoreConnectionPNames;
@@ -54,42 +52,6 @@ public class PortalHttpClient {
 		} catch (Exception e) {
 			logger.error("An error occurred: {}", e.getMessage(), e);
 			return new DefaultHttpClient();
-		}
-	}
-
-	public static HttpClient getCloseableHttpClient() {
-		try {
-			// Initialize truststore and custom socket factory (sf)
-			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-			trustStore.load(null, null);
-			MySSLSocketFactory sf = new MySSLSocketFactory(trustStore);
-			sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-
-			// Create HttpParams and SchemeRegistry
-			HttpParams params = new BasicHttpParams();
-			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-			HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
-
-			SchemeRegistry registry = new SchemeRegistry();
-			registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-			registry.register(new Scheme("https", sf, 443));
-
-			// Initialize ClientConnectionManager
-			ClientConnectionManager ccm = new ThreadSafeClientConnManager(params, registry);
-
-			// Use try-with-resources to ensure httpClient is closed after use
-			try (CloseableHttpClient httpClient = new DefaultHttpClient(ccm, params)) {
-				int timeout = 40; // seconds
-				HttpParams httpParams = httpClient.getParams();
-				httpParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, timeout * 1000);
-				httpParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, timeout * 1000);
-				httpParams.setParameter(ClientPNames.CONN_MANAGER_TIMEOUT, (long) timeout * 1000);
-
-				return httpClient;
-			}
-		} catch (Exception e) {
-			logger.error("An error occurred: {}", e.getMessage(), e);
-			return HttpClients.createDefault();
 		}
 	}
 }
--- src/test/java/net/lacnic/portal/auth/client/PortalHttpClientTest.java
+++ src/test/java/net/lacnic/portal/auth/client/PortalHttpClientTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpParams;
 import org.junit.jupiter.api.Test;
@@ -29,22 +28,6 @@ public class PortalHttpClientTest {
 	}
 
 	@Test
-	void testGetCloseableHttpClient_returnsConfiguredHttpClient() {
-		// Call the method
-		HttpClient httpClient = PortalHttpClient.getCloseableHttpClient();
-
-		// Verify that HttpClient is not null and is an instance of CloseableHttpClient
-		assertNotNull(httpClient);
-		assertTrue(httpClient instanceof CloseableHttpClient, "Expected instance of CloseableHttpClient");
-
-		// Check if parameters were set correctly
-		HttpParams httpParams = httpClient.getParams();
-		assertEquals(40000, httpParams.getIntParameter("http.connection.timeout", 0));
-		assertEquals(40000, httpParams.getIntParameter("http.socket.timeout", 0));
-		assertEquals(40000L, httpParams.getLongParameter("http.conn-manager.timeout", 0));
-	}
-
-	@Test
 	void testGetNewHttpClient_fallbackOnException() {
 		HttpClient httpClient = null;
 		try {
@@ -55,18 +38,5 @@ public class PortalHttpClientTest {
 		// Verify that HttpClient is not null even if exceptions occur
 		assertNotNull(httpClient);
 		assertTrue(httpClient instanceof DefaultHttpClient);
-	}
-
-	@Test
-	void testGetCloseableHttpClient_fallbackOnException() {
-		HttpClient httpClient = null;
-		try {
-			httpClient = PortalHttpClient.getCloseableHttpClient();
-		} catch (Exception ignored) {
-		}
-
-		// Verify that HttpClient is not null even if exceptions occur
-		assertNotNull(httpClient);
-		assertTrue(httpClient instanceof CloseableHttpClient);
 	}
 }
